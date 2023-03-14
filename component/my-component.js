c884b68f86b01a8379f7437b1884b8be4d587e42
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2023-03-14 17:17:10 -0500

    feat: :memo: v-if v-else v-else-if
    Usamos las directivas condicionales v-if v-else v-else-if

--- component/my-component.js
+++ component/my-component.js
@@ -1,7 +1,12 @@
 export default{
     data(){
         return {
-            centinela: false
+            mensaje1: `<em>Apolo</em>`,
+            mensaje2: `<mark>Artemis</mark>`,
+            mensaje3: `<u>Sputnik</u>`,
+            mensaje4: `No cumple`,
+            opcion: null,
+
         }
     }
 }
